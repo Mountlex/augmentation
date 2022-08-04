50f6d7e27d19907fd331b1fcf811c7e3156c3b2d
Alexander Lindermayr <alexander.lindermayr97@gmail.com>
2022-08-04 21:20:20 +0200

    .

--- src/nice_path.rs
+++ src/nice_path.rs
@@ -1,7 +1,6 @@
 use itertools::Itertools;
 use num_rational::Rational64;
 
-
 use crate::{
     comps::{Component, CreditInvariant, EdgeType, Graph},
     edges_of_type, enumerate_and_check, merge_graphs, relabel_nodes,
@@ -36,14 +35,6 @@ struct NicePath {
     last: Node,
 }
 
-fn all_tuples<T: Eq + Copy>(list: Vec<T>) -> Vec<(T, T)> {
-    if list[0] != list[1] {
-        vec![(list[0], list[1]), (list[1], list[0])]
-    } else {
-        vec![(list[0], list[1])]
-    }
-}
-
 fn sum_of_credits<C: CreditInvariant>(nodes: Vec<&Node>, credit_inv: C) -> Rational64 {
     nodes
         .into_iter()
@@ -54,7 +45,6 @@ fn sum_of_credits<C: CreditInvariant>(nodes: Vec<&Node>, credit_inv: C) -> Ratio
 pub fn prove_nice_path_progress<C: CreditInvariant>(comps: Vec<Component>, credit_inv: C) {
     for f in &comps {
         let nf = Node::Any(f.clone());
-
         for p in &comps {
             let np = if let Component::Simple(_) = p {
                 Node::NicePair(p.clone())
@@ -99,36 +89,51 @@ fn prove_nice_path<C: CreditInvariant>(path: NicePath, credit_inv: C) -> bool {
     // dbg!("{:?}", Dot::with_config(&prelast_graph, &[Config::EdgeNoLabel]));
     // dbg!("{:?}", Dot::with_config(&last_graph, &[Config::EdgeNoLabel]));
 
-    for f_perm in f_nodes.iter().combinations_with_replacement(2) {
-        for (&f_out, &f_in) in all_tuples(f_perm) {
-            for p_perm in p_nodes.iter().combinations_with_replacement(2) {
-                for (&p_out, &p_in) in all_tuples(p_perm) {
-                    for l_perm in l_nodes.iter().combinations_with_replacement(2) {
-                        for (&l_out, &l_in) in all_tuples(l_perm) {
-                            let cycle = vec![
-                                (f_out, l_in, EdgeType::Zero),
-                                (l_out, p_in, EdgeType::Zero),
-                                (p_out, f_in, EdgeType::Zero),
-                            ];
-                            let sellable = edges_of_type(&graph, EdgeType::One);
-                            let previous_credits = sum_of_credits(
-                                vec![&path.first, &path.prelast, &path.last],
-                                credit_inv.clone(),
-                            );
+    for f_perm in f_nodes
+        .iter()
+        .combinations_with_replacement(2)
+        .permutations(2)
+        .flatten()
+        .dedup()
+    {
+        let (&f_out, &f_in) = (f_perm[0], f_perm[1]);
+        for p_perm in p_nodes
+            .iter()
+            .combinations_with_replacement(2)
+            .permutations(2)
+            .flatten()
+            .dedup()
+        {
+            let (&p_out, &p_in) = (p_perm[0], p_perm[1]);
+            for l_perm in l_nodes
+                .iter()
+                .combinations_with_replacement(2)
+                .permutations(2)
+                .flatten()
+                .dedup()
+            {
+                let (&l_out, &l_in) = (l_perm[0], l_perm[1]);
+                let cycle = vec![
+                    (f_out, l_in, EdgeType::Zero),
+                    (l_out, p_in, EdgeType::Zero),
+                    (p_out, f_in, EdgeType::Zero),
+                ];
+                let sellable = edges_of_type(&graph, EdgeType::One);
+                let previous_credits = sum_of_credits(
+                    vec![&path.first, &path.prelast, &path.last],
+                    credit_inv.clone(),
+                );
 
-                            let result = enumerate_and_check(
-                                &graph,
-                                vec![cycle].into_iter(),
-                                sellable.into_iter().powerset(),
-                                credit_inv.clone(),
-                                previous_credits,
-                            );
-                            if !result {
-                                //println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
-                                return false;
-                            }
-                        }
-                    }
+                let result = enumerate_and_check(
+                    &graph,
+                    vec![cycle].into_iter(),
+                    sellable.into_iter().powerset(),
+                    credit_inv.clone(),
+                    previous_credits,
+                );
+                if !result {
+                    //println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+                    return false;
                 }
             }
         }
