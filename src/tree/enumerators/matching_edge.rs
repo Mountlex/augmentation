cde15b3002fe9d9e97955a1cfbb53eb24ff9370f
Alexander Lindermayr <linderal@uni-bremen.de>
2022-10-05 10:29:54 +0200

    fixed matching edges

--- src/tree/enumerators/matching_edge.rs
+++ src/tree/enumerators/matching_edge.rs
@@ -1,4 +1,3 @@
-
 use itertools::Itertools;
 
 use crate::{
@@ -10,7 +9,7 @@ use crate::{
 };
 
 pub struct MatchingEnum {
-    size: usize
+    size: usize,
 }
 
 impl MatchingEnum {
@@ -21,7 +20,7 @@ impl MatchingEnum {
 
 pub struct MatchingEnumerator<'a> {
     instance: &'a TreeCaseInstance,
-    size: usize
+    size: usize,
 }
 
 impl<'a> Enumerator<TreeCaseInstance, TreeContext> for MatchingEnumerator<'a> {
@@ -30,14 +29,28 @@ impl<'a> Enumerator<TreeCaseInstance, TreeContext> for MatchingEnumerator<'a> {
         let edges = self.instance.edges_between(last_idx);
         let left = &self.instance.comps[last_idx - 1];
         let right = &self.instance.comps[last_idx];
-        let left_matched = edges
+        let mut left_matched = edges
             .iter()
             .flat_map(|e| e.endpoint_at(last_idx - 1))
             .collect_vec();
-        let right_matched = edges
+        let mut right_matched = edges
             .iter()
             .flat_map(|e| e.endpoint_at(last_idx))
             .collect_vec();
+
+        let mut left_fix = 0;
+        let mut right_fix = 0;
+        if last_idx == 1 {
+            // left is leaf
+            if !left_matched.contains(&left.fixed_node()) {
+                left_matched.push(left.fixed_node());
+                left_fix += 1;
+            }
+        }
+        if !right_matched.contains(&right.fixed_node()) {
+            right_matched.push(right.fixed_node());
+            right_fix += 1;
+        }
 
         let left_free: Box<dyn Iterator<Item = &Node>> = if let Component::Large(n) = left {
             Box::new(std::iter::repeat(n).take(self.size))
@@ -61,20 +74,33 @@ impl<'a> Enumerator<TreeCaseInstance, TreeContext> for MatchingEnumerator<'a> {
         };
 
         let instance = self.instance.clone();
-       
-        let iter = left_free.combinations(self.size).flat_map(move |lefts| {
-            let right_free = right_free.clone();
-            let instance = instance.clone();
-            right_free.into_iter().permutations(self.size).map(move |rights| {
-                let mut instance_clone = instance.clone();
-                for (l,r) in lefts.iter().zip(rights.into_iter()) {
-                    instance_clone
-                    .edges
-                    .push(Edge::new(**l, last_idx - 1, r, last_idx));
-                }
-                instance_clone
-            })
-        });
+
+        let iter = left_free
+            .permutations(self.size - left_fix)
+            .flat_map(move |lefts| {
+                let right_free = right_free.clone();
+                let instance = instance.clone();
+                right_free
+                    .into_iter()
+                    .permutations(self.size - right_fix)
+                    .map(move |rights| {
+                        let mut instance_clone = instance.clone();
+                        for (l, r) in lefts
+                            .iter()
+                            .chain(std::iter::once(&&left.fixed_node()).take(left_fix))
+                            .zip(
+                                rights
+                                    .into_iter()
+                                    .chain(std::iter::once(right.fixed_node()).take(right_fix)),
+                            )
+                        {
+                            instance_clone
+                                .edges
+                                .push(Edge::new(**l, last_idx - 1, r, last_idx));
+                        }
+                        instance_clone
+                    })
+            });
 
         Box::new(iter)
     }
@@ -88,7 +114,10 @@ impl EnumeratorTactic<TreeCaseInstance, TreeCaseInstance, TreeContext> for Match
     }
 
     fn get_enumerator<'a>(&'a self, data: &'a TreeCaseInstance) -> Self::Enumer<'a> {
-        MatchingEnumerator { instance: data, size: self.size }
+        MatchingEnumerator {
+            instance: data,
+            size: self.size,
+        }
     }
 
     fn item_msg(&self, item: &TreeCaseInstance) -> String {
