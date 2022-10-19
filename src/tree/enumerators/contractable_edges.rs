8ec96cf0cf017778f041bc7a6e367ed6ee07cfe2
Alexander Lindermayr <linderal@uni-bremen.de>
2022-10-19 22:00:32 +0200

    only add new edges

--- src/tree/enumerators/contractable_edges.rs
+++ src/tree/enumerators/contractable_edges.rs
@@ -14,12 +14,20 @@ pub struct ContractableEdgesEnumerator<'a> {
 
 impl<'a> Enumerator<TreeCaseInstance, TreeContext> for ContractableEdgesEnumerator<'a> {
     fn iter(&self, _context: &TreeContext) -> Box<dyn Iterator<Item = TreeCaseInstance> + '_> {
-        let iter = self.instance.free_nodes.iter().combinations(2).map(|e| {
-            let mut instance_clone = self.instance.instance.clone();
-            let new_edge = Edge::from_tuple(*e[0], *e[1]);
-            instance_clone.edges.push(new_edge);
-            instance_clone
-        });
+        let comp = &self.instance.instance.comps[self.instance.contr_idx];
+
+        let iter = self
+            .instance
+            .free_nodes
+            .iter()
+            .combinations(2)
+            .filter(|e| !comp.is_adjacent(e[0], e[1]))
+            .map(|e| {
+                let mut instance_clone = self.instance.instance.clone();
+                let new_edge = Edge::from_tuple(*e[0], *e[1]);
+                instance_clone.edges.push(new_edge);
+                instance_clone
+            });
 
         Box::new(iter)
     }
