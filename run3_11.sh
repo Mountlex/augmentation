d6025332febb53dca152ddde7958c23e29bcd1ea
Alexander Lindermayr <linderal@uni-bremen.de>
2023-04-19 14:14:28 +0200

    3_11

--- run3_11.sh
+++ run3_11.sh
@@ -1,8 +1,8 @@
 cargo run --release -- path 3 11 c3 -o proofs  -n 3 -e 1 
-
 cargo run --release -- path 3 11 c5 -o proofs  -n 3 -e 5
 cargo run --release -- path 3 11 c6 -o proofs  -n 1 -e 3
 cargo run --release -- path 3 11 c7 -o proofs  -n 1 -e 3
 cargo run --release -- path 3 11 l -o proofs  -n 3 -e 2
 cargo run --release -- path 3 11 cp -o proofs  -n 3 -e 6
 cargo run --release -- path 3 11 ct -o proofs  -n 3 -e 6
+cargo run --release -- path 3 11 c4 -o proofs --sc -n 3 -e 6 -i 2
