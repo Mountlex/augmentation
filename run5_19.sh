d380ce77e9f743fb8245bc60ff22810a5879e92c
Alexander Lindermayr <linderal@uni-bremen.de>
2023-04-27 13:52:21 +0200

    .

---
+++ run5_19.sh
@@ -0,0 +1,7 @@
+cargo run --release -- path 5 19 c4 -o proofs --sc -n 3 -e 6 -i 2
+cargo run --release -- path 5 19 c5 -o proofs  -n 3 -e 5
+cargo run --release -- path 5 19 c6 -o proofs  -n 1 -e 3
+cargo run --release -- path 5 19 c7 -o proofs  -n 1 -e 3
+cargo run --release -- path 5 19 l -o proofs  -n 3 -e 2
+cargo run --release -- path 5 19 cp -o proofs  -n 3 -e 6
+cargo run --release -- path 5 19 ct -o proofs  -n 3 -e 6
