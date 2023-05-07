9e0b5d0a812ab7f939c2b7a2e88f288abd683374
Alexander Lindermayr <linderal@uni-bremen.de>
2023-05-07 11:32:21 +0200

    .

--- src/path/enumerators/edges.rs
+++ src/path/enumerators/edges.rs
@@ -48,6 +48,8 @@ fn enumerate_parts(instance: &Instance) -> Option<(Box<dyn Iterator<Item = InstP
     let outside_edges = instance.out_edges();
     let all_edges = instance.all_edges();
     let npc = instance.npc();
+    let outside_used_for_gain = instance.used_for_credit_gain();
+
 
     let mut nodes_to_pidx: Vec<Option<Pidx>> = vec![None; old_path_len * 20];
     for path_comp in &path_comps {
@@ -103,115 +105,6 @@ fn enumerate_parts(instance: &Instance) -> Option<(Box<dyn Iterator<Item = InstP
         if let Some(iter) = ensure_three_matching(comp_nodes, other_nodes, instance) {
             let iter = to_cases(iter, nodes_to_pidx, instance);
             return Some((iter, format!("3-Matching of {} first pathnodes", s)));
-        }
-    }
-
-    // Prio 3.5: 4-matching
-    for s in 2..=len - 1 {
-        let comp_nodes = path_comps
-            .iter()
-            .take(s)
-            .flat_map(|c| c.comp.matching_nodes().to_vec())
-            .collect_vec();
-
-        let size: usize = path_comps
-            .iter()
-            .take(s)
-            .map(|comp| comp.comp.num_vertices())
-            .sum();
-
-        let other_nodes = path_comps
-            .iter()
-            .filter(|p| p.path_idx.raw() >= s)
-            .flat_map(|p| p.comp.matching_nodes().to_vec())
-            .collect_vec();
-        if size >= 10 {
-            if let Some(iter) = ensure_k_matching(comp_nodes, other_nodes, instance, 4) {
-                let iter = to_cases(iter, nodes_to_pidx, instance);
-                return Some((iter, format!("4-Matching of {} first pathnodes", s)));
-            }
-        }
-    }
-
-    // Prio 4: Edges due to contractablility
-    for path_comp in path_comps.iter().take(len - 1) {
-        let idx = path_comp.path_idx;
-        if let Some(iter) = handle_contractable_components(path_comp, instance) {
-            let iter = to_cases(iter, nodes_to_pidx, instance);
-            return Some((iter, format!("Contractablility of {}", idx)));
-        }
-    }
-
-    let outside_used_for_gain = instance.used_for_credit_gain();
-
-    // Prio 5: Larger contractable comps
-    let all_edges = instance.all_edges();
-    let relevant_comps = path_comps
-        .iter()
-        .filter(|c| c.comp.is_c3() || c.comp.is_c4() || c.comp.is_c5() || c.comp.is_c6())
-        .cloned()
-        .collect_vec();
-    for i in 3..=3 {
-        // println!("Instance: {}", instance);
-        for pc in pseudo_cycles_of_length(
-            relevant_comps.clone(),
-            None,
-            all_edges.clone(),
-            vec![],
-            i,
-            false,
-        ) {
-            //  println!("{}", pc);
-            let mut vertices_sets = vec![vec![]];
-            for (n1, c, n2) in pc.cycle {
-                let idx = c.to_idx();
-                let comp = relevant_comps
-                    .iter()
-                    .find(|comp| comp.path_idx == idx)
-                    .unwrap();
-                if n1 == n2 {
-                    vertices_sets.iter_mut().for_each(|set| set.push(n1));
-                } else if comp.comp.is_adjacent(&n1, &n2) {
-                    vertices_sets.iter_mut().for_each(|set| {
-                        set.append(&mut comp.comp.nodes().iter().cloned().collect_vec())
-                    });
-                } else {
-                    let (p1, p2) = comp.comp.paths_between(&n1, &n2);
-                    // println!("set {:?}, p1 {:?}, p2 {:?}", vertices_sets, p1, p2);
-                    vertices_sets = vertices_sets
-                        .into_iter()
-                        .flat_map(|set| {
-                            vec![
-                                vec![set.clone(), p1.clone()].concat(),
-                                vec![set, p2.clone()].concat(),
-                            ]
-                        })
-                        .collect_vec();
-                }
-            }
-
-            for set in vertices_sets {
-                if let Some(good_verts) = is_contractible(&set, instance) {
-                    // G[set] is contractible
-                    let all_nodes = instance
-                        .path_nodes()
-                        .flat_map(|c| c.comp.matching_nodes())
-                        .filter(|n| !set.contains(n) || good_verts.contains(n))
-                        .cloned()
-                        .collect_vec();
-
-                    // We now enumerate all edges which potentially resolve the contractability. Those are between the good vertices and
-                    // (all vertices / bad vertices) (including good vertices)
-                    let iter = edge_iterator(good_verts, all_nodes, true, true).unwrap();
-
-                    let iter = to_cases(iter, nodes_to_pidx, instance);
-                    //println!("iter: {}", iter.iter().join(", "));
-                    return Some((
-                        Box::new(iter),
-                        format!("Contractablility of large set {:?}", set),
-                    ));
-                }
-            }
         }
     }
 
@@ -397,6 +290,116 @@ fn enumerate_parts(instance: &Instance) -> Option<(Box<dyn Iterator<Item = InstP
         }
     }
 
+
+    // Prio 3.5: 4-matching
+    for s in 2..=len - 1 {
+        let comp_nodes = path_comps
+            .iter()
+            .take(s)
+            .flat_map(|c| c.comp.matching_nodes().to_vec())
+            .collect_vec();
+
+        let size: usize = path_comps
+            .iter()
+            .take(s)
+            .map(|comp| comp.comp.num_vertices())
+            .sum();
+
+        let other_nodes = path_comps
+            .iter()
+            .filter(|p| p.path_idx.raw() >= s)
+            .flat_map(|p| p.comp.matching_nodes().to_vec())
+            .collect_vec();
+        if size >= 10 {
+            if let Some(iter) = ensure_k_matching(comp_nodes, other_nodes, instance, 4) {
+                let iter = to_cases(iter, nodes_to_pidx, instance);
+                return Some((iter, format!("4-Matching of {} first pathnodes", s)));
+            }
+        }
+    }
+
+    // Prio 4: Edges due to contractablility
+    for path_comp in path_comps.iter().take(len - 1) {
+        let idx = path_comp.path_idx;
+        if let Some(iter) = handle_contractable_components(path_comp, instance) {
+            let iter = to_cases(iter, nodes_to_pidx, instance);
+            return Some((iter, format!("Contractablility of {}", idx)));
+        }
+    }
+
+
+    // Prio 5: Larger contractable comps
+    let all_edges = instance.all_edges();
+    let relevant_comps = path_comps
+        .iter()
+        .filter(|c| c.comp.is_c3() || c.comp.is_c4() || c.comp.is_c5() || c.comp.is_c6())
+        .cloned()
+        .collect_vec();
+    for i in 3..=3 {
+        // println!("Instance: {}", instance);
+        for pc in pseudo_cycles_of_length(
+            relevant_comps.clone(),
+            None,
+            all_edges.clone(),
+            vec![],
+            i,
+            false,
+        ) {
+            //  println!("{}", pc);
+            let mut vertices_sets = vec![vec![]];
+            for (n1, c, n2) in pc.cycle {
+                let idx = c.to_idx();
+                let comp = relevant_comps
+                    .iter()
+                    .find(|comp| comp.path_idx == idx)
+                    .unwrap();
+                if n1 == n2 {
+                    vertices_sets.iter_mut().for_each(|set| set.push(n1));
+                } else if comp.comp.is_adjacent(&n1, &n2) {
+                    vertices_sets.iter_mut().for_each(|set| {
+                        set.append(&mut comp.comp.nodes().iter().cloned().collect_vec())
+                    });
+                } else {
+                    let (p1, p2) = comp.comp.paths_between(&n1, &n2);
+                    // println!("set {:?}, p1 {:?}, p2 {:?}", vertices_sets, p1, p2);
+                    vertices_sets = vertices_sets
+                        .into_iter()
+                        .flat_map(|set| {
+                            vec![
+                                vec![set.clone(), p1.clone()].concat(),
+                                vec![set, p2.clone()].concat(),
+                            ]
+                        })
+                        .collect_vec();
+                }
+            }
+
+            for set in vertices_sets {
+                if let Some(good_verts) = is_contractible(&set, instance) {
+                    // G[set] is contractible
+                    let all_nodes = instance
+                        .path_nodes()
+                        .flat_map(|c| c.comp.matching_nodes())
+                        .filter(|n| !set.contains(n) || good_verts.contains(n))
+                        .cloned()
+                        .collect_vec();
+
+                    // We now enumerate all edges which potentially resolve the contractability. Those are between the good vertices and
+                    // (all vertices / bad vertices) (including good vertices)
+                    let iter = edge_iterator(good_verts, all_nodes, true, true).unwrap();
+
+                    let iter = to_cases(iter, nodes_to_pidx, instance);
+                    //println!("iter: {}", iter.iter().join(", "));
+                    return Some((
+                        Box::new(iter),
+                        format!("Contractablility of large set {:?}", set),
+                    ));
+                }
+            }
+        }
+    }
+
+    
     None
 }
 
