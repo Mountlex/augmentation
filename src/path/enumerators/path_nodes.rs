b8fc4dc876b9fd290f4b7ee74a55700bf90a824a
Alexander Lindermayr <alexander.lindermayr97@gmail.com>
2023-06-02 09:36:39 +0200

    refactor path enumerator

--- src/path/enumerators/path_nodes.rs
+++ src/path/enumerators/path_nodes.rs
@@ -1,23 +1,23 @@
 use itertools::Itertools;
 
 use crate::{
-    comps::Component,
     path::{
         enumerators::edges::edge_enumerator,
         instance::{InstPart, Instance, PathNode},
+        path_definition::valid_in_out_pre_npc,
         PathComp, Pidx,
     },
     types::Edge,
     util::relabels_nodes_sequentially,
-    Node,
 };
 
 /// Splits the current pattern by adding one more component and considering all feasible in- out-
 /// pairs between the previous first component and the new component
 pub fn path_comp_enumerator(instance: &Instance) -> Box<dyn Iterator<Item = InstPart>> {
     let pattern_comps = instance.path_nodes().cloned().collect_vec();
-    let pattern_edges = instance.all_edges();
     let all_comps = instance.context.comps.iter().cloned().collect_vec();
+
+    assert!(!pattern_comps.is_empty());
 
     // Create a new case for every possible new component
     let iter = all_comps.into_iter().flat_map(move |new_comp| {
@@ -68,16 +68,13 @@ pub fn path_comp_enumerator(instance: &Instance) -> Box<dyn Iterator<Item = Inst
             comp.in_nodes().to_vec()
         };
 
-        let comp_filter = comp.clone();
-        let comp_map = comp;
-        let node_map = node;
-
         // for all in_nodes of the new component
         let iter: Box<dyn Iterator<Item = PathComp>> =
             Box::new(in_nodes.into_iter().flat_map(move |in_node| {
-                let comp_filter = comp_filter.clone();
-                let comp_map = comp_map.clone();
-                let node_map = node_map.clone();
+                // copies for moves
+                let comp_filter = comp.clone();
+                let comp = comp.clone();
+                let node = node.clone();
 
                 // for all valid out_nodes of the new component
                 let iter: Box<dyn Iterator<Item = PathComp>> = Box::new(
@@ -86,68 +83,26 @@ pub fn path_comp_enumerator(instance: &Instance) -> Box<dyn Iterator<Item = Inst
                         .clone()
                         .into_iter()
                         .filter(move |out_node| {
-                            prevalid_in_out(&comp_filter, in_node, *out_node, new_node_idx.is_prelast())
+                            // only consider in-out combination which are possible in nice paths
+                            valid_in_out_pre_npc(
+                                &comp_filter,
+                                in_node,
+                                *out_node,
+                                new_node_idx.is_prelast(),
+                            )
                         })
                         .flat_map(move |out_node| {
-                            let new_nice_pairs = comp_map.edges();
-                            let mut path_comp = PathComp {
-                                comp: comp_map.clone(),
+                            let initial_nps = comp.edges();
+                            let path_comp = PathComp {
+                                comp: comp.clone(),
                                 in_node: Some(in_node),
                                 out_node: Some(out_node),
-                                used: node_map.is_used(),
+                                used: node.is_used(),
                                 path_idx: new_node_idx,
-                                initial_nps: new_nice_pairs,
+                                initial_nps,
                             };
 
-                            if (comp_map.is_c4()
-                                || (comp_map.is_c5()
-                                    && !node_map.is_used()
-                                    && new_node_idx.is_prelast()))
-                                && !comp_map.is_adjacent(&in_node, &out_node)
-                            {
-                                // in and out must form a nice pair in comp!
-                                if comp_map.is_c4() {
-                                    path_comp.initial_nps.push((in_node, out_node));
-                                }
-
-                                if comp_map.is_c5() {
-                                    //
-                                    //    out
-                                    //   /   \
-                                    //   1   2
-                                    //   |   |
-                                    //  in - 3
-                                    //
-                                    let v1 = *comp_map
-                                        .nodes()
-                                        .iter()
-                                        .find(|v| {
-                                            comp_map.is_adjacent(v, &in_node)
-                                                && comp_map.is_adjacent(v, &out_node)
-                                        })
-                                        .unwrap();
-                                    let v2 = *comp_map
-                                        .nodes()
-                                        .iter()
-                                        .find(|v| **v != v1 && comp_map.is_adjacent(v, &out_node))
-                                        .unwrap();
-                                    let v3 = *comp_map
-                                        .nodes()
-                                        .iter()
-                                        .find(|v| **v != v1 && comp_map.is_adjacent(v, &in_node))
-                                        .unwrap();
-
-                                    path_comp.initial_nps.push((in_node, out_node));
-                                    let mut p1 = path_comp.clone();
-                                    let mut p2 = path_comp.clone();
-
-                                    p1.initial_nps.push((v3, out_node));
-                                    p2.initial_nps.push((v2, in_node));
-
-                                    return vec![p1, p2].into_iter();
-                                }
-                            }
-                            vec![path_comp].into_iter()
+                            split_cases_by_required_nice_pairs(path_comp)
                         }),
                 );
                 iter
@@ -159,14 +114,63 @@ pub fn path_comp_enumerator(instance: &Instance) -> Box<dyn Iterator<Item = Inst
     Box::new(iter)
 }
 
-// TODO READ
+fn split_cases_by_required_nice_pairs(mut path_comp: PathComp) -> impl Iterator<Item = PathComp> {
+    let comp = &path_comp.comp;
+    let used = path_comp.used;
+    let idx = path_comp.path_idx;
+    let in_node = path_comp.in_node.unwrap();
+    let out_node = path_comp.out_node.unwrap();
+
+    // if in and out are adjacent we already have a nice pair
+    if !comp.is_adjacent(&in_node, &out_node) {
+        if comp.is_c4() {
+            path_comp.initial_nps.push((in_node, out_node));
+        }
+
+        if comp.is_c5() && !used && idx.is_prelast() {
+            //
+            //    out
+            //   /   \
+            //   1   2
+            //   |   |
+            //  in - 3
+            //
+            let v1 = *comp
+                .nodes()
+                .iter()
+                .find(|v| comp.is_adjacent(v, &in_node) && comp.is_adjacent(v, &out_node))
+                .unwrap();
+            let v2 = *comp
+                .nodes()
+                .iter()
+                .find(|v| **v != v1 && comp.is_adjacent(v, &out_node))
+                .unwrap();
+            let v3 = *comp
+                .nodes()
+                .iter()
+                .find(|v| **v != v1 && comp.is_adjacent(v, &in_node))
+                .unwrap();
+
+            path_comp.initial_nps.push((in_node, out_node));
+            let mut p1 = path_comp.clone();
+            let mut p2 = path_comp.clone();
+
+            p1.initial_nps.push((v3, out_node));
+            p2.initial_nps.push((v2, in_node));
+
+            return vec![p1, p2].into_iter();
+        }
+    }
+    vec![path_comp].into_iter()
+}
+
 pub fn path_extension_enumerator(
     instance: &mut Instance,
 ) -> Option<(Box<dyn Iterator<Item = InstPart>>, String)> {
-    let path_comps = instance.path_nodes().cloned().collect_vec();
-    let rem_edges = instance.rem_edges();
+    let pattern_comps = instance.path_nodes().cloned().collect_vec();
+    let back_edges = instance.rem_edges();
 
-    if rem_edges.is_empty() && path_comps.len() >= 3 {
+    if back_edges.is_empty() && pattern_comps.len() >= 3 {
         // If we cannot find more edges, and there are no rem edges, it wont help to enumerate more nodes.
         if edge_enumerator(instance, false).is_none() {
             log::info!("Enumerating more path nodes does not help!");
@@ -177,39 +181,39 @@ pub fn path_extension_enumerator(
     //let profile = instance.get_profile(true);
     //log::info!("Currently extending: {}", profile);
 
-    let old_path_len = path_comps.len();
+    let old_pattern_len = pattern_comps.len();
 
     // Enumerate components and in / out
     let iter = path_comp_enumerator(instance);
 
-    // Enumerate REM edges at new component
+    // Enumerate back edges which might hit or don't hit the new component
     let iter: Box<dyn Iterator<Item = InstPart>> =
         Box::new(iter.into_iter().flat_map(move |inst_part| {
-            let rem_edges_copy = rem_edges.iter().cloned().collect_vec();
-            rem_edges_copy
+            let back_edges = back_edges.iter().cloned().collect_vec();
+            back_edges
                 .into_iter()
                 .powerset()
-                .flat_map(move |rem_edges_hit_new_node| {
+                .flat_map(move |hitting_back_edges| {
                     let path_comp = inst_part.path_nodes.first().unwrap().clone();
-                    let node_idx = inst_part.path_nodes.first().unwrap().path_idx;
+                    let new_idx = inst_part.path_nodes.first().unwrap().path_idx;
 
-                    // rem_edges_hit_new_node is the set of edges which now should hit node_idx
+                    // hitting_back_edges is the set of edges which should now hit the newly enumerated comp
                     let mut iter: Box<dyn Iterator<Item = InstPart>> =
                         Box::new(vec![InstPart::new_path_comp(path_comp.clone())].into_iter());
-                    for i in 0..old_path_len {
+           
+                    for i in 0..old_pattern_len {
                         let source_idx = Pidx::from(i);
                         let comp = path_comp.comp.clone();
 
                         // all matching edges between source_idx and node_idx
-                        let matching_edges = rem_edges_hit_new_node
+                        let matching_edges = hitting_back_edges
                             .clone()
                             .into_iter()
                             .filter(|e| e.source_idx == source_idx)
                             .collect_vec();
 
                         // previous rem_edges which will be now realized are converted to non_rem_edges, so we collect those ids
-                        let non_rem_edges =
-                            rem_edges_hit_new_node.iter().map(|e| e.id).collect_vec();
+                        let non_rem_edges = hitting_back_edges.iter().map(|e| e.id).collect_vec();
 
                         iter = Box::new(iter.flat_map(move |inst_part| {
                             let matching_edges = matching_edges.clone();
@@ -219,7 +223,7 @@ pub fn path_extension_enumerator(
                             comp.combinations_with_replacement(matching_edges.len())
                                 .into_iter()
                                 .filter(move |matched| {
-                                    if source_idx.prec() == node_idx {
+                                    if source_idx.prec() == new_idx {
                                         if let Some(out) = path_comp.out_node {
                                             out.is_comp()
                                                 || matched.iter().all(|matched| *matched != out)
@@ -234,7 +238,7 @@ pub fn path_extension_enumerator(
                                     let mut edges = matched
                                         .into_iter()
                                         .zip(matching_edges.iter())
-                                        .map(|(u, v)| Edge::new(v.source, source_idx, u, node_idx))
+                                        .map(|(u, v)| Edge::new(v.source, source_idx, u, new_idx))
                                         .collect_vec();
 
                                     let mut non_rem_edges = non_rem_edges.clone();
@@ -253,14 +257,4 @@ pub fn path_extension_enumerator(
         }));
 
     Some((Box::new(iter), "path node".into()))
-}
-
-fn prevalid_in_out(c: &Component, new_in: Node, new_out: Node, prelast: bool) -> bool {
-    if c.is_c3() || c.is_c4() || (c.is_c5() && prelast) {
-        new_in != new_out
-    // } else if c.is_complex() {
-    //     new_in != new_out || new_in.is_comp()
-    } else {
-        true
-    }
 }
--- src/path/path_definition.rs
+++ src/path/path_definition.rs
@@ -22,3 +22,14 @@ pub fn valid_in_out_npc(
         true
     }
 }
+
+
+pub fn valid_in_out_pre_npc(c: &Component, new_in: Node, new_out: Node, prelast: bool) -> bool {
+    if c.is_c3() || c.is_c4() || (c.is_c5() && prelast) {
+        new_in != new_out
+    // } else if c.is_complex() {
+    //     new_in != new_out || new_in.is_comp()
+    } else {
+        true
+    }
+}
