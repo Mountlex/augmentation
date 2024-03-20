fd1bca6b6fb62e4aca7caf9cbae7bc86fadb5837
Alexander Lindermayr <linderal@uni-bremen.de>
2024-03-20 17:09:00 +0100

    .

--- run1_4.sh
+++ run1_4.sh
@@ -1,5 +1,5 @@
-cargo run --release -- path 1 4 c5 -o proofs   -i 2
-cargo run --release -- path 1 4 c6 -o proofs   -i 1
-cargo run --release -- path 1 4 c7 -o proofs   -i 1
-cargo run --release -- path 1 4 l  -o proofs   -i 1
-cargo run --release -- path 1 4 c4 -o proofs  -i 1
+cargo run --release -- path 1 4  c5 -o proofs   -i 1
+cargo run --release -- path 1 4  c6 -o proofs   -i 1
+cargo run --release -- path 1 4  c7 -o proofs   -i 1
+cargo run --release -- path 1 4  l  -o proofs   -i 1
+cargo run --release -- path 1 4  c4 -o proofs  -i 2
